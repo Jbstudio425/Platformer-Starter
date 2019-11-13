e12dafbcd33f4f7d19632edcb5d927fb375211a8
Emil Gustafsson <emiltgustafsson@hotmail.com>
2019-11-13 15:38:37 +0100

    Project

---
+++ LavapotionWorkTest/Assets/Scripts/Character.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Character : MonoBehaviour
+{
+    Rigidbody2D _rigidbody;
+    float _movementSpeed = 20;
+    float _jumpVelocity = 40;
+    
+    void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+    
+    void Update()
+    {
+        var velocity = Vector2.zero;
+        var scale = transform.localScale;
+        if (Input.GetKey(KeyCode.A))
+        {
+            velocity.x += -1;
+            scale.x = -1;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            velocity.x += 1;
+            scale.x = 1;
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            velocity.y += _jumpVelocity;
+        }
+
+
+        _rigidbody.velocity += velocity * (_movementSpeed * Time.deltaTime);
+        transform.localScale = scale;
+    }
+}

8b53d9eabc8285f53aee9656844f7656ab4da73f
Emil Gustafsson <emiltgustafsson@hotmail.com>
2019-11-16 15:54:20 +0100

    Save System

--- LavapotionWorkTest/Assets/Scripts/Collector.cs
+++ LavapotionWorkTest/Assets/Scripts/Collector.cs
@@ -7,7 +7,7 @@ public class Collector : MonoBehaviour
 {
     private int points = 0;
     [SerializeField] private Text scoreText = null;
-    
+
     void Awake()
     {
         if(scoreText != null) scoreText.text = points.ToString();
@@ -16,6 +16,12 @@ public class Collector : MonoBehaviour
     public void AddPoints(int points)
     {
         this.points += points;
+        if(scoreText != null) scoreText.text = this.points.ToString();
+    }
+
+    public void SetPoints(int points)
+    {
+        this.points  = points;
         if(scoreText != null) scoreText.text = this.points.ToString();
     }
 
--- LavapotionWorkTest/Assets/Scripts/GameHandler.cs
+++ LavapotionWorkTest/Assets/Scripts/GameHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class GameHandler : MonoBehaviour
 {
@@ -10,24 +11,50 @@ public class GameHandler : MonoBehaviour
     {
         playerObject = GameObject.FindGameObjectWithTag("Player");
 
-        SaveObject save = new SaveObject {
-            playerScore = playerObject.GetComponent<Collector>().,
-            playerPosition = new Vector3(0,0,0)
-        };
-
-        string json = JsonUtility.ToJson(save);
-        Debug.Log(json);
-
-        SaveObject loadedSaveObject = JsonUtility.FromJson<SaveObject>(json);
-        Debug.Log(loadedSaveObject.playerPosition);
+        SaveSystem.Init();
     }
 
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.F5)){
+            Save();
+        }
+
+        if(Input.GetKeyDown(KeyCode.F8)){
+            Load();
+        }
     }
 
-    public class SaveObject
+    private void Load()
+    {
+        string saveString = SaveSystem.Load();
+        
+        if(saveString != null){
+            SaveData saveObj = JsonUtility.FromJson<SaveData>(saveString);
+
+            playerObject.transform.position = saveObj.playerPosition;
+            playerObject.GetComponent<Collector>().SetPoints(saveObj.playerScore);
+        }
+        else{
+            Debug.Log("No save file found!");
+        }
+    }
+
+    private void Save()
+    {
+        Vector3 position = playerObject.transform.position;
+        int score = playerObject.GetComponent<Collector>().GetPoints();
+
+        SaveData saveObj = new SaveData {
+            playerScore = score,
+            playerPosition = position
+        };
+
+        string json = JsonUtility.ToJson(saveObj);
+        SaveSystem.Save(json);
+    }
+
+    public class SaveData
     {
         public int playerScore = 0;
         public Vector3 playerPosition = Vector3.zero;
